824994c38c8d19d17903cdf3089c66070ba7aa63
LuisFilipeMendonca <filipedev@outlook.pt>
2021-02-09 14:41:57 +0000

    Added graph traversal

--- DataStructures/graph.js
+++ DataStructures/graph.js
@@ -34,20 +34,85 @@ class Graph {
 
     delete this.adjacencyList[vertex];
   }
+
+  dfsRecursive(start) {
+    const result = [];
+    const visited = {};
+
+    function dfsHelper(graph, vertex) {
+      visited[vertex] = true;
+      result.push(vertex);
+
+      if (graph[vertex].length <= 0) return;
+
+      for (let i = 0; i < graph[vertex].length; i++) {
+        if (!visited[graph[vertex][i]]) {
+          dfsHelper(graph, graph[vertex][i]);
+        }
+      }
+    }
+    dfsHelper(this.adjacencyList, start);
+
+    return result;
+  }
+
+  dfsIterative(start) {
+    const stack = [start];
+    const result = [];
+    const visited = {};
+    let currentVertex;
+
+    visited[start] = true;
+
+    while (stack.length) {
+      currentVertex = stack.pop();
+      result.push(currentVertex);
+
+      for (let i = 0; i < this.adjacencyList[currentVertex].length; i++) {
+        const neighbor = this.adjacencyList[currentVertex][i];
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          stack.push(neighbor);
+        }
+      }
+    }
+    return result;
+  }
+
+  bfs(start) {
+    const result = [];
+    const queue = [start];
+    const visited = {};
+    visited[start] = true;
+    let currentVertex;
+
+    while (queue.length) {
+      currentVertex = queue.shift();
+      result.push(currentVertex);
+
+      this.adjacencyList[currentVertex].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          queue.push(neighbor);
+        }
+      });
+    }
+    return result;
+  }
 }
 
-const graph = new Graph();
-graph.addVertex("Tokyo");
-graph.addVertex("Dallas");
-graph.addVertex("Aspen");
-graph.addVertex("Hong Kong");
-graph.addVertex("Los Angels");
-graph.addEdge("Tokyo", "Dallas");
-graph.addEdge("Tokyo", "Hong Kong");
-graph.addEdge("Dallas", "Aspen");
-graph.addEdge("Dallas", "Hong Kong");
-graph.addEdge("Dallas", "Los Angels");
-graph.addEdge("Hong Kong", "Los Angels");
-console.log(graph.adjacencyList);
-graph.removeVertex("Tokyo");
-console.log(graph.adjacencyList);
+const g = new Graph();
+g.addVertex("A");
+g.addVertex("B");
+g.addVertex("C");
+g.addVertex("D");
+g.addVertex("E");
+g.addVertex("F");
+g.addEdge("A", "B");
+g.addEdge("A", "C");
+g.addEdge("B", "D");
+g.addEdge("C", "E");
+g.addEdge("D", "E");
+g.addEdge("D", "F");
+g.addEdge("E", "F");
+console.log(g.bfs("A"));
